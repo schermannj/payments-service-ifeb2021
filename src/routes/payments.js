984b3ce5ab37732747e4c50baf3553e90b02d7a1
vsheruda <v.sheruda@aimprosoft.com>
2021-02-09 19:22:45 +0200

    Refactor payments, move out router function out of router definition

--- src/routes/payments.js
+++ src/routes/payments.js
@@ -2,10 +2,11 @@ const router = require('express').Router();
 const db = require('../db');
 const payments = require('../services/payments.service')(db);
 const { success, failure } = require('../utils/router.utils');
+const { parseIntParam } = require('../middlewares');
 
 const NAMESPACE = '/payments';
 
-router.get(NAMESPACE, async (req, res) => {
+const list = async (req, res) => {
   try {
     const data = await payments.list();
 
@@ -13,19 +14,23 @@ router.get(NAMESPACE, async (req, res) => {
   } catch (e) {
     failure(res, e.message);
   }
-});
+};
 
-router.get(`${NAMESPACE}/:id`, async ({ params: { id } }, res) => {
+const get = async ({ params: { id } }, res) => {
   try {
     const data = await payments.get(id);
 
-    success(res, data);
-  } catch (e) {
-    failure(res, e.message);
-  }
-});
+    if (data?.length === 0) {
+      return failure(res, `No payments found with id = ${id}`, 404);
+    }
 
-router.post(NAMESPACE, async (req, res) => {
+    return success(res, data);
+  } catch (e) {
+    return failure(res, e.message);
+  }
+};
+
+const create = async (req, res) => {
   try {
     // TODO: joi schema
     const data = payments.create(req.body);
@@ -34,9 +39,9 @@ router.post(NAMESPACE, async (req, res) => {
   } catch (e) {
     failure(res, e.message);
   }
-});
+};
 
-router.patch(`/${NAMESPACE}/:id`, async ({ params: { id }, body }, res) => {
+const update = async ({ params: { id }, body }, res) => {
   try {
     // TODO: joi schema
     const data = await payments.update(id, body);
@@ -45,9 +50,9 @@ router.patch(`/${NAMESPACE}/:id`, async ({ params: { id }, body }, res) => {
   } catch (e) {
     failure(res, e.message);
   }
-});
+};
 
-router.delete(`/${NAMESPACE}/:id`, async ({ params: { id } }, res) => {
+const remove = async ({ params: { id } }, res) => {
   try {
     await payments.remove(id);
 
@@ -55,6 +60,12 @@ router.delete(`/${NAMESPACE}/:id`, async ({ params: { id } }, res) => {
   } catch (e) {
     failure(res, e.message);
   }
-});
+};
+
+router.get(NAMESPACE, list);
+router.get(`${NAMESPACE}/:id`, parseIntParam('id'), get);
+router.post(NAMESPACE, create);
+router.patch(`/${NAMESPACE}/:id`, parseIntParam('id'), update);
+router.delete(`/${NAMESPACE}/:id`, parseIntParam('id'), remove);
 
 module.exports = router;
